1284220648bfb6228dd0c47f8baeeaf181381f9a
Lubomir Goychev <lubomir.goychev@gmail.com>
2017-04-22 15:35:44 +0300

    add blog project

---
+++ Blog-Skeleton/NUnitTestProject2/NUnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//using NUnit.Framework;
+
+namespace NUnitTestProject2
+{
+    //[TestFixture]
+    public class NUnitTest1
+    {
+        //[Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
